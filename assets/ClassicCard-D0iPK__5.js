f8c584e4bd5c648883b75bb7b787ce1501f5afd5
thehoach <thehoachbk@gmail.com>
2025-08-09 21:30:02 +0700

    Updates

---
+++ assets/ClassicCard-D0iPK__5.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-C-9nPV3l.js";function i(s){return r.jsxs("div",{children:[r.jsx("h2",{children:"Received Props"}),r.jsx("pre",{children:JSON.stringify(s,null,2)})]})}export{i as default};
