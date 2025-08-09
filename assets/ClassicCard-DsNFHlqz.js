13c4b0199953ee69900f786bb1874c8a857dc7c8
thehoach <thehoachbk@gmail.com>
2025-08-09 21:05:08 +0700

    Updates

---
+++ assets/ClassicCard-DsNFHlqz.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-DTT_PEK6.js";function i(s){return r.jsxs("div",{children:[r.jsx("h2",{children:"Received Props"}),r.jsx("pre",{children:JSON.stringify(s,null,2)})]})}export{i as default};
